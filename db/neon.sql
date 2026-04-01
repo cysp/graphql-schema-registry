8c08678a583578f0623724c9ca671590870247c0
Scott Talbot <s@chikachow.org>
2026-04-01 22:18:03 +1100

    chore(db): ignore neon-specific privilege grants

--- db/neon.sql
+++ db/neon.sql
@@ -1,8 +1,2 @@
-CREATE ROLE cloud_admin WITH SUPERUSER;
-
-CREATE ROLE neon_superuser WITH NOLOGIN IN ROLE cloud_admin NOINHERIT;
-
 \i schema.sql
 \i schema-constraints.sql
-
-GRANT ALL ON ALL TABLES IN SCHEMA public TO neon_superuser;
