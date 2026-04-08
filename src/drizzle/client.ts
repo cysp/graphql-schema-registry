652e5536f506f227c9ed7b3e92f3f76cce9e19cc
Scott Talbot <s@chikachow.org>
2026-04-08 15:39:58 +1000

    chore(drizzle): use drizzle configuration during schema export

--- src/drizzle/client.ts
+++ src/drizzle/client.ts
@@ -1,9 +1,10 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import type postgres from "postgres";
 
+import drizzleConfig from "../../drizzle.config.ts";
 import * as schema from "./schema.ts";
 import type { PostgresJsDatabase } from "./types.ts";
 
 export function createDrizzleClient({ client }: { client: postgres.Sql }): PostgresJsDatabase {
-  return drizzle({ client, schema });
+  return drizzle({ client, schema, casing: drizzleConfig.casing });
 }
