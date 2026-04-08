781dee59d880f4385c71ad71e246761046c12b9d
Scott Talbot <s@chikachow.org>
2026-04-08 16:17:45 +1000

    chore(drizzle): remove database url from drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,9 @@
-// oxlint-disable import/no-default-export, no-process-env
+// oxlint-disable import/no-default-export
 
 import type { Config } from "drizzle-kit";
 
-const databaseUrl = process.env["DATABASE_URL"];
-
 export default {
   casing: "snake_case",
-  dbCredentials: {
-    url: databaseUrl,
-  },
   dialect: "postgresql",
   out: "./drizzle",
   schema: "./src/drizzle/schema.ts",
